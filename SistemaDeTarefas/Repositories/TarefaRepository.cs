0de5957d59288dd75d0c9eb58d7f47a9bc0534e8
Igor Bonfim <igorbonfim88@gmail.com>
2024-08-04 18:28:34 -0300

    API Sistema de Tarefas

---
+++ SistemaDeTarefas/Repositories/TarefaRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositories.Interfaces;
+
+namespace SistemaDeTarefas.Repositories;
+
+public class TarefaRepository : ITarefaRepository
+{
+    private readonly SistemaDeTarefasDbContext _dbContext;
+
+    public TarefaRepository(SistemaDeTarefasDbContext sistemaDeTarefasDbContext)
+    {
+        _dbContext = sistemaDeTarefasDbContext;
+    }
+
+    public async Task<TarefaModel> BuscarPorId(int id) => await _dbContext.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task<List<TarefaModel>> BuscarTarefas()
+    {
+        return await _dbContext.Tarefas.ToListAsync();
+    }
+
+    public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
+    {
+        await _dbContext.Tarefas.AddAsync(tarefa);
+        await _dbContext.SaveChangesAsync();
+        return tarefa;
+    }
+
+    public async Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
+    {
+        TarefaModel tarefaPorId = await BuscarPorId(id);
+
+        if (tarefaPorId == null)
+        {
+            throw new Exception($"A tarefa pro ID: {id} não foi encontrada no banco de dados");
+        }
+
+        tarefaPorId.Nome = tarefa.Nome;
+        tarefaPorId.Descricao = tarefa.Descricao;
+        tarefaPorId.Status = tarefa.Status;
+        tarefaPorId.UsuarioID = tarefa.UsuarioID;        
+
+        _dbContext.Tarefas.Update(tarefaPorId);
+        await _dbContext.SaveChangesAsync();
+
+        return tarefaPorId;
+    }
+
+    public async Task<bool> Apagar(int id)
+    {
+        TarefaModel tarefaPorId = await BuscarPorId(id);
+
+        if (tarefaPorId == null)
+        {
+            throw new Exception($"A tarefa pro ID: {id} não foi encontrada no banco de dados");
+        }
+
+        _dbContext.Tarefas.Remove(tarefaPorId);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }       
+}
