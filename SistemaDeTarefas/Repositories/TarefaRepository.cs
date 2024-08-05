4d883138edd171516f014384b33dbbc906d42cd1
Igor Bonfim <igorbonfim88@gmail.com>
2024-08-04 21:48:56 -0300

    Finalizando vinculo do usuario com a tarefa

--- SistemaDeTarefas/Repositories/TarefaRepository.cs
+++ SistemaDeTarefas/Repositories/TarefaRepository.cs
@@ -14,11 +14,17 @@ public class TarefaRepository : ITarefaRepository
         _dbContext = sistemaDeTarefasDbContext;
     }
 
-    public async Task<TarefaModel> BuscarPorId(int id) => await _dbContext.Tarefas.FirstOrDefaultAsync(x => x.Id == id);
-
+    public async Task<TarefaModel> BuscarPorId(int id)
+    {
+        return await _dbContext.Tarefas
+            .Include(x => x.Usuario)
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
     public async Task<List<TarefaModel>> BuscarTarefas()
     {
-        return await _dbContext.Tarefas.ToListAsync();
+        return await _dbContext.Tarefas
+            .Include (x => x.Usuario)
+            .ToListAsync();
     }
 
     public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
